658d316d6adcc18d47c27571620525efcf817aff
AndreyMQU <andrey.yermolenko@students.mq.edu.au>
2026-02-28 16:09:26 +1100

    A Screen with a Scaffold and Center Widgets
    I added widgets in the scaffold that put "comp3130" in white in the middle of the screen

--- week1comp3130/lib/main.dart
+++ week1comp3130/lib/main.dart
@@ -9,6 +9,15 @@ class Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: Colors.red);
+    return const Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Text(
+          'comp3130',
+          style: TextStyle(fontSize: 30, color: Colors.white),
+        ),
+      ),
+    
+    );
   }
 }
